0f1d65eb6deb520baa7a7aa1fd8d6e9892252bba
Kenny <khnsmnplwn@gmail.com>
2025-01-28 23:30:57 +0000

    began unit testing

--- src/lib/utils.test.ts
+++ src/lib/utils.test.ts
@@ -10,3 +10,59 @@ test("tableFormToDatabaseForm_Empty_Empty", () =>{
 
     expect(tableFormToDatabaseForm(inputTable, firstDate)).toEqual(expected)
 })
+
+test("tableFormToDatabaseForm_AllFalse_Empty", () =>{
+
+    const inputTable:boolean[][] = [new Array<boolean>(48).fill(false)]
+    const firstDate = new Date(0)
+    
+    const expected:Availability[] = []
+
+    expect(tableFormToDatabaseForm(inputTable, firstDate)).toEqual(expected)
+})
+
+test("tableFormToDatabaseForm_ThreeConsecutiveTrue_OneAvailability", () =>{
+
+    const inputTable:boolean[][] = [new Array<boolean>(48).fill(false)]
+
+    inputTable[0].fill(true, 0, 3)
+
+    const firstDate = new Date("1970-01-01T00:00:00")
+    
+    const expected:Availability[] = [ {start: new Date("1970-01-01T00:00:00"), end: new Date("1970-01-01T01:30:00")}]
+
+    expect(tableFormToDatabaseForm(inputTable, firstDate)).toEqual(expected)
+})
+
+test("tableFormToDatabaseForm_2SetsOfThreeConsecutiveTrue_TwoAvailabilitise", () =>{
+
+    const inputTable:boolean[][] = [new Array<boolean>(48).fill(false)]
+
+    inputTable[0].fill(true, 0, 3)
+    inputTable[0].fill(true, 6, 9)
+
+    const firstDate = new Date("1970-01-01T00:00:00")
+    
+    const expected:Availability[] = [ 
+        {start: new Date("1970-01-01T00:00:00"), end: new Date("1970-01-01T01:30:00")},
+        {start: new Date("1970-01-01T03:00:00"), end: new Date("1970-01-01T04:30:00")},
+    ]
+
+    expect(tableFormToDatabaseForm(inputTable, firstDate)).toEqual(expected)
+})
+
+test("tableFormToDatabaseForm_ThreeConsecutiveAcrossDay_OneAvailability", () =>{
+
+    const inputTable:boolean[][] = [new Array<boolean>(48).fill(false), new Array<boolean>(48).fill(false)]
+
+    inputTable[0].fill(true, 46)
+    inputTable[1][0] = true
+
+    const firstDate = new Date("1970-01-01T00:00:00")
+    
+    const expected:Availability[] = [ 
+        {start: new Date("1970-01-01T23:00:00"), end: new Date("1970-01-01T00:30:00")},
+    ]
+
+    expect(tableFormToDatabaseForm(inputTable, firstDate)).toEqual(expected)
+})
